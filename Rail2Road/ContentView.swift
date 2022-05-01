2a6e2a69fb67e0287e4324b3aa5aa6f3d1766687
Sanav Pai <sanavpaipka@gmail.com>
2022-04-30 22:10:29 -0500

    Initial Commit

---
+++ Rail2Road/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rail2Road
+//
+//  Created by Pranav Sai on 4/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rail2Road/Rail2RoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rail2RoadApp.swift
+//  Rail2Road
+//
+//  Created by Pranav Sai on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Rail2RoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
