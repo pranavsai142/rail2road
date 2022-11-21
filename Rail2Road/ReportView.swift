b2ab66fb41e000401ff76766d526fce4033174cd
Pranav Sai <pranavsai142@gmail.com>
2022-11-20 21:07:46 -0800

    Reformat ReportView to select a time duration 	-https://stackoverflow.com/questions/30771820/how-to-convert-timeinterval-into-minutes-seconds-and-milliseconds-in-swift

--- Rail2Road/ReportView.swift
+++ Rail2Road/ReportView.swift
@@ -7,18 +7,82 @@
 
 import SwiftUI
 
+extension TimeInterval {
+    var hourMinuteSecondMS: String {
+        String(format:"%d:%02d:%02d.%03d", hour, minute, second, millisecond)
+    }
+    var minuteSecondMS: String {
+        String(format:"%d:%02d.%03d", minute, second, millisecond)
+    }
+    var hour: Int {
+        Int((self/3600).truncatingRemainder(dividingBy: 3600))
+    }
+    var minute: Int {
+        Int((self/60).truncatingRemainder(dividingBy: 60))
+    }
+    var second: Int {
+        Int(truncatingRemainder(dividingBy: 60))
+    }
+    var millisecond: Int {
+        Int((self*1000).truncatingRemainder(dividingBy: 1000))
+    }
+}
+
 struct ReportView: View {
     @EnvironmentObject var database: FireDatabaseReference
     @EnvironmentObject var dataConglomerate: DataConglomerate
     
-    @State private var startDate = Date()
+    @State private var startDate = Calendar.current.date(byAdding: .hour, value: -1, to: Date())!
     @State private var endDate = Date()
     
     var uid: String
     var railyard: Railyard
     
+    var minDateConstraints: ClosedRange<Date> {
+        //Min date constraints
+        let minDate = Calendar.current.date(byAdding: .hour, value: -12, to: Date())!
+        let maxDate = Calendar.current.date(byAdding: .minute, value: -1, to: Date())!
+        return minDate...maxDate
+    }
+    
+    var maxDateConstraints: ClosedRange<Date> {
+        //Max date constraints
+        let minDate = startDate
+        let maxDate = Date()
+        return minDate...maxDate
+    }
+    
     private func submit() {
         
+    }
+    
+
+    private func timeIntervalToString(delta: TimeInterval) -> String {
+        if(delta <= 1) {
+            return "Invalid Input"
+        }
+        
+        var returnString = ""
+        
+        if(delta.hour == 1) {
+            returnString = returnString + String(delta.hour) + " hour"
+        } else if(delta.hour > 1) {
+            returnString = returnString + String(delta.hour) + " hours"
+        }
+        
+        if(delta.minute == 1) {
+            returnString = returnString + " " + String(delta.minute) + " minute"
+        } else if(delta.minute > 1) {
+            returnString = returnString + " " + String(delta.minute) + " minutes"
+        }
+
+//        if(delta.second == 1) {
+//            returnString = returnString + " " + String(delta.second) + " second"
+//        } else if(delta.second > 1) {
+//            returnString = returnString + " " + String(delta.second) + " seconds"
+//        }
+        
+        return returnString
     }
     
     var body: some View {
@@ -26,14 +90,19 @@ struct ReportView: View {
             Text("Truthful reporting delivers accurate data!")
                 .italic()
                 .font(.caption)
-                .multilineTextAlignment(.center)
-            DatePicker("Start:", selection: $startDate)
-            DatePicker("End:", selection: $endDate)
+            Divider()
+            DatePicker("Start:", selection: $startDate, in: minDateConstraints, displayedComponents: [.date, .hourAndMinute])
+            DatePicker("End:", selection: $endDate, in: maxDateConstraints, displayedComponents: [.date, .hourAndMinute])
             Spacer()
-            Button(action: {
-                submit()
-            }) {
-                Text("submit")
+            HStack {
+                Text("Wait Duration: \(timeIntervalToString(delta: (startDate.distance(to: endDate))))")
+                    .bold()
+                Spacer()
+                Button(action: {
+                    submit()
+                }) {
+                    Text("submit")
+                }
             }
         }
             .padding()
